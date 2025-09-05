b69c08eca507ce27cb1d9c7ebc2609010c5b3592
devCaiodias <caiodiasmartins12@gmail.com>
2025-09-05 18:16:53 -0300

    fix: simplify database connection configuration by using a single DB_CONFIG URL

--- backend/src/database/database.providers.ts
+++ backend/src/database/database.providers.ts
@@ -13,11 +13,7 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: process.env.DB_HOST,
-        port: 5432,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        url: process.env.DB_CONFIG,
         entities: [User, Project, Task, RevokedToken],
         synchronize: true,
       });
