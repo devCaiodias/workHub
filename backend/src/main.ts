35bdf4acb08bd39579f91b1d15f5b416144349dd
devCaiodias <caiodiasmartins12@gmail.com>
2025-09-07 16:22:12 -0300

    fix: update CORS configuration to allow requests from the production URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
 
   app.enableCors({
-    origin: 'https://work-hub-sigma.vercel.app/',
+    origin: 'https://work-hub-sigma.vercel.app',
     credentials: true,
   })
 
