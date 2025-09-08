ee8e7bf4876fc7cb1ab3917a38af361baeab9c66
devCaiodias <caiodiasmartins12@gmail.com>
2025-09-08 10:09:31 -0300

    fix: refactor CORS configuration to define allowed origins within the callback

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,15 +11,22 @@ async function bootstrap() {
   ];
 
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, origin);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  });
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'https://work-hub-sigma.vercel.app',
+    ];
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
 
   app.useGlobalPipes(new ValidationPipe);
   await app.listen(process.env.PORT ?? 8080);
